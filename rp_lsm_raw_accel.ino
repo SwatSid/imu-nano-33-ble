d050eee1c1adbb6b3a4c4ff208c8c29867d94cd2
MSI\adsid <a.d.siddhaarthan@gmail.com>
2021-01-04 10:03:17 -0800

    Base Code

---
+++ rp_lsm_raw_accel.ino
@@ -0,0 +1,44 @@
+/*
+
+   Test Program to find Roll and Pitch from just Accelerometer values from the onboard LSM IMU of Arduino Nano 33 BLE
+
+*/
+
+#include <Arduino_LSM9DS1.h>
+
+float x, y, z;
+float roll, pitch;
+
+void setup() {
+  Serial.begin(9600);
+
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
+
+//  Serial.print("Accelerometer sample rate = ");
+//  Serial.print(IMU.accelerationSampleRate());
+//  Serial.println(" Hz");
+//  Serial.println();
+}
+
+void loop() {
+
+if (IMU.accelerationAvailable()) {
+    IMU.readAcceleration(x, y, z);
+
+//    Serial.print(x);
+//    Serial.print('\t');
+//    Serial.print(y);
+//    Serial.print('\t');
+//    Serial.println(z);
+
+  roll = atan2(x,z) * 180/PI;
+  pitch = atan2(y,z) * 180/PI;
+
+  Serial.print(roll);
+  Serial.print('\t');
+  Serial.println(pitch);
+  }
+}
